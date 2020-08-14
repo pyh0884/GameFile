3328ef8099c20af880643706a505d42c6a2ada1a
pyh0884 <pyh2084@hotmail.com>
2020-08-15 00:19:01 +0800

    Import Rewired Plugin, Add Root Menu folders

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerID = 10;
+    private Player player;
+    void Start()
+    {
+        player = ReInput.players.GetPlayer(playerID);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (player.GetButtonDown("Item2"))
+        //{
+        //    UseItem(items[1]);
+        //    items[1] = 0;
+        //    FindObjectOfType<TopCanvas>().ChangeItem(playerID, items);
+        //}
+        //movement.x = player.GetAxisRaw("MoveHorizontal");
+        //movement.y = player.GetAxisRaw("MoveVertical");
+    }
+}
