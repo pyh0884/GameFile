e6ab860730f3f2ee22175cfba59aad9f9c946049
pyh0884 <pyh2084@hotmail.com>
2020-08-15 12:42:17 +0800

    PauseMenu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Rewired;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public bool isPaused;
+    public GameObject pauseMenu;
     private void Awake()
     {
         if (instance == null)
@@ -15,10 +17,30 @@ public class GameManager : MonoBehaviour
             return;
         }
         DontDestroyOnLoad(gameObject);
+        pauseMenu.SetActive(false);
     }
-
+    public void PauseToggle()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+            isPaused = false;
+        }
+        //Unpause
+        else 
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+            isPaused = true;
+        }
+        //pause
+    }
     void Update()
     {
-        
+        if (ReInput.players.GetSystemPlayer().GetButtonDown("Cancel"))
+        {
+            PauseToggle();
+        }
     }
 }
