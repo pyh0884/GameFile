4780b9f92f08723790fec09986144ca6397b4168
pyh0884 <pyh2084@hotmail.com>
2020-08-15 12:50:11 +0800

    Bug fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,7 +17,6 @@ public class GameManager : MonoBehaviour
             return;
         }
         DontDestroyOnLoad(gameObject);
-        pauseMenu.SetActive(false);
     }
     public void PauseToggle()
     {
