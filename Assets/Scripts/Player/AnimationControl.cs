58c2d5bb1fdf9a8e09d985ff24ac6d6dfa484edb
XiangmingKA <715524834@qq.com>
2020-08-16 16:22:21 +0800

    Add PlayerWithAnimation Prefab

---
+++ Assets/Scripts/Player/AnimationControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour
+{
+    public float idleToRunSpeed = 1.0f;
+    private Rigidbody rg;
+    public Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = GetComponentInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 vol = rg.velocity;
+        float speed = Mathf.Abs(vol.x) + Mathf.Abs(vol.y) + Mathf.Abs(vol.z);
+        if(speed > idleToRunSpeed)
+        {
+            ani.SetBool("running", true);
+        }
+        else
+        {
+            ani.SetBool("running", false);
+        }
+    }
+}
