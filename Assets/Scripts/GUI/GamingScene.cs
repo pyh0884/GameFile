e70d93a6863d81bd8fa28713bd0375b443164511
pyh0884 <pyh2084@hotmail.com>
2020-08-16 10:24:13 +0800

    Store Logic

--- Assets/Scripts/GUI/GamingScene.cs
+++ Assets/Scripts/GUI/GamingScene.cs
@@ -10,22 +10,30 @@ public class GamingScene : MonoBehaviour
 {
     public Text coinsText1;
     public Text coinsText2;
+    public Text foodText1;
+    public Text foodText2;
     public Transform health1;
     public Transform health2;
 
     public int Coins1;
     public int Coins2;
+    public int Food1;
+    public int Food2;
+    private int coinsCollected = 0;
     public int TotalCoins = 15;
     void Start()
     {
         Coins1 = 0;
         Coins2 = 0;
+        Food1 = 0;
+        Food2 = 0;
         RefreshCoins();
+        RefreshFood();
     }
 
     void Update()
     {
-        if (Coins1 + Coins2 >= TotalCoins)
+        if (coinsCollected >= TotalCoins)
         {
             FindObjectOfType<GameManager>().Win();
         }
@@ -38,6 +46,7 @@ public class GamingScene : MonoBehaviour
             Coins1 += num;
         else
             Coins2 += num;
+        if (num > 0) coinsCollected += num;
         RefreshCoins();
     }
     void RefreshCoins()
@@ -45,7 +54,46 @@ public class GamingScene : MonoBehaviour
         coinsText1.text = "X " + Coins1;
         coinsText2.text = "X " + Coins2;
     }
-
+    void RefreshFood()
+    {
+        if (Food1 > 0)
+        {
+            foodText1.text = "Have Food!";
+        }
+        else 
+        {
+            foodText1.text = "No Food!";
+        }
+        if (Food2 > 0)
+        {
+            foodText2.text = "Have Food!";
+        }
+        else
+        {
+            foodText2.text = "No Food!";
+        }
+    }
+    public void checkCoins(int id, int num)
+    {
+        if (id == 0 && Food1 <= 0 && Coins1 >= num)
+        {
+            AddCoin(-num, id);
+            addFood(id, 1);
+        }
+        else if (id == 1 && Food2 <= 0 && Coins2 >= num)
+        {
+            AddCoin(-num, id);
+            addFood(id, 1);
+        }
+    }
+    public void addFood(int id, int num) 
+    {
+        if (id == 0)
+            Food1 += num;
+        else if (id == 1)
+            Food2 += num;
+        RefreshFood();
+    }
     void RefreshHealth()
     {
         for(int i = 0;i< health1.childCount;i++)
